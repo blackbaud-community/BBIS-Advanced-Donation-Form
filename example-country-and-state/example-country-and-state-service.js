2f6c52ea8e79104b7d910b4caeeed1867a708562
Bobby Earl <bobby.earl@blackbaud.com>
2015-04-19 22:48:07 -0400

    Create example-country-and-state-service.js

---
+++ example-country-and-state/example-country-and-state-service.js
@@ -0,0 +1,31 @@
+// Let's be good developers and not pollute the global namespace
+(function($) {
+
+// Let's make sure the DOM is ready
+$(function() {
+
+  var selectCountry = $('#country');
+  var selectState = $('#state');
+  var service = new BLACKBAUD.api.CountryService();
+  
+  // Load Countries
+  service.getCountries(function(countries) {
+    for (var i = 0, j = countries.length; i < j; i++) {
+      selectCountry.append('<option value="' + countries[i].Id + '">' + countries[i].Description + '</option>');
+    }
+  });
+  
+  // Watch Countries Change
+  $('.countries').on('change', function() {
+  
+    // Load States
+    service.getStates($(this).val(), function(states) {
+      selectState.html('');
+      for (var i = 0, j = states.length; i < j; i++) {
+        selectState.append('<option value="' + states[i].Id + '">' + states[i].Description + '</option>');
+      }
+    });
+  });
+
+});
+}(jQuery));
